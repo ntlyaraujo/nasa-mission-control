ab656bfe14ae40d874d945ae371110e3afd4ce8b
Nataly Araujo <ntlyaraujo@gmail.com>
2024-01-09 09:17:37 +0100

    Initial mongoose setup and schemas

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,29 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
+
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = 'mongodb+srv://ntlyaraujo:Z8mzOg2whO6hy2ef@cluster0.lnwomqs.mongodb.net/?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
         console.log(`Server is running on port: ${PORT}`);
+    });
+
+    mongoose.connection.once('connected', () => {
+        console.log('Mongoose is connected!');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.error(err);
     });
 }
 

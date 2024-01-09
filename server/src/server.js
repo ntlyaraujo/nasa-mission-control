d0cec291654def27475e62199d59c0af28c26b3f
Nataly Araujo <ntlyaraujo@gmail.com>
2024-01-09 14:07:27 +0100

    Update mongoose event emitter and hide password

--- server/src/server.js
+++ server/src/server.js
@@ -5,9 +5,16 @@ const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = 'mongodb+srv://ntlyaraujo:Z8mzOg2whO6hy2ef@cluster0.lnwomqs.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = `mongodb+srv://ntlyaraujo:${process.env.MONGO_PASSWORD}@cluster0.lnwomqs.mongodb.net/?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
+
+mongoose.connection.once('connected', () => {
+    console.log('Mongoose is connected!');
+});
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
 
 
 async function startServer() {
@@ -18,13 +25,7 @@ async function startServer() {
         console.log(`Server is running on port: ${PORT}`);
     });
 
-    mongoose.connection.once('connected', () => {
-        console.log('Mongoose is connected!');
-    });
 
-    mongoose.connection.on('error', (err) => {
-        console.error(err);
-    });
 }
 
 startServer();
